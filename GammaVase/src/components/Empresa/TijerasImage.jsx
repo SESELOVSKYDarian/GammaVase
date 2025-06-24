841049e85681a0f68dc99ef819b7d8c76f4ff9fc
SESELOVSKYDarian <darianseselovsky@gmail.com>
2025-06-24 14:25:01 -0300

    Cambios dentro de Empresa

--- GammaVase/src/components/Empresa/TijerasImage.jsx
+++ GammaVase/src/components/Empresa/TijerasImage.jsx
@@ -6,7 +6,7 @@ const TijerasImage = () => {
   return (
     <div className="tijeras-container">
       {/* Sustituye 'tijeras.png' por tu imagen local */}
-      <img src="/ruta/a/tijeras.png" alt="Tijeras cortando" />
+      <img src="/assets/tijerasgamma.png" alt="Tijeras cortando" />
     </div>
   );
 };
