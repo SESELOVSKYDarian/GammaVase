afa8f5c8aaae6292b1aac164f9cefb87af2a10f9
VALLEJOS_Alexis <100102557+AlexisVallejos@users.noreply.github.com>
2025-06-26 11:04:00 -0300

    Footer final

--- GammaVase/src/components/footer.jsx
+++ GammaVase/src/components/footer.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import './Footer.css';
+// asumimos que ya está el CSS preparado
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         {/* Logo */}
         <div className="footer-logo">
-          <img src="/logo-gamma.png" alt="Gamma Modas" />
+          <img src="/logo.png" alt="Gamma Modas" />
         </div>
 
         {/* Contacto */}
         <div className="footer-contact">
           <div className="footer-item">
-            <img src="/whatsapp-icon.png" alt="WhatsApp" className="footer-icon" />
+            <img src="/wasap.svg" alt="WhatsApp" className="footer-icon" />
             <span>+54 (223) 633-4301</span>
           </div>
           <div className="footer-item">
-            <img src="/mail-icon.png" alt="Email" className="footer-icon" />
+            <img src="/gmail.png" alt="Email" className="footer-icon" />
             <span>ventas@gammodas.com.ar</span>
           </div>
         </div>
